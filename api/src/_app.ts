0eb196512d76077dbb690b8085def80e71ad69e0
guhcalm <guhcalm@outlook.com>
2022-10-11 11:36:11 -0400

    vanilla-api

--- api/src/_app.ts
+++ api/src/_app.ts
@@ -1,3 +1,18 @@
-const { log } = console
+import { createServer } from "node:http"
+const { stringify } = JSON
 
-log("hii from api")
+const { PORT = 3000 } = process.env
+
+createServer(({ method, url }, res) => {
+  res
+    .setHeader("content-type", "application/json")
+    .setHeader("access-controll-allow-origin", "*")
+    .setHeader("access-controll-allow-method", "*")
+  try {
+    res.end(stringify({}[method + "." + url] || { message: "default message" }))
+  } catch (exception) {
+    exception instanceof Error
+      ? res.end(stringify({ error: exception.message }))
+      : res.end(stringify({ error: "Internal Server Error", exception }))
+  }
+}).listen(PORT)
